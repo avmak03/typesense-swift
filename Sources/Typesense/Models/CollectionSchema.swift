b3214c9d610ed5f84e165f0150cfaf7949c2627f
Sabesh Bharathi <sabesh5601@gmail.com>
2021-11-30 16:34:03 +0530

    Remove API directory and associated older files

--- Sources/Typesense/Models/CollectionSchema.swift
+++ Sources/Typesense/Models/CollectionSchema.swift
@@ -16,7 +16,6 @@ public struct CollectionSchema: Codable {
     /** A list of fields for querying, filtering and faceting */
     public var fields: [Field]
     /** The name of an int32 / float field that determines the order in which the search results are ranked when a sort_by clause is not provided during searching. This field must indicate some kind of popularity. */
-
     public var defaultSortingField: String?
     /** List of symbols or special characters to be used for  splitting the text into individual words in addition to space and new-line characters.  */
     public var tokenSeparators: [String]?
