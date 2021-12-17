7261bc286b1d4b24a4ef089f6ab3359108c4fbc5
programVeins <sabesh5601@gmail.com>
2021-12-17 09:48:13 +0530

    Add Linker flags for concurrency

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Typesense",
-            dependencies: []),
+            dependencies: [],
+            linkerSettings: [LinkerSetting.unsafeFlags(["-v", "-Wl", "-weak-lswift_Concurrency -Wl", "-rpath", "/usr/lib/swift"
+                                                       ])]
+        ),
         .testTarget(
             name: "TypesenseTests",
             dependencies: ["Typesense"]),
