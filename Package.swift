c58c3487093a6244aabfc77a6a76ee923b212cb8
avmak03 <96456247+avmak03@users.noreply.github.com>
2021-12-25 10:42:46 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5.0
+// swift-tools-version:5.4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Typesense",
     platforms: [
-        .iOS(.v15), .macOS(.v12)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
